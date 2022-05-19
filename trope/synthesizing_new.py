3f3b67e9889890611f9f0ebeeb5a073d65569f78
Tim Nilsen <nilsentim.e@gmail.com>
2022-05-19 10:54:06 -0700

    completed vectorizing getting the duration in samples

--- trope/synthesizing_new.py
+++ trope/synthesizing_new.py
@@ -31,43 +31,42 @@ class Tone:
         sine = np.sin(2 * np.pi * self.each_sample * self.frequency / self.sample_rate) * self.amplitude
         return sine
 
+
 class Synthesis:
 
     def __init__(
         self,
         input_refrain,
-        durations,
+        input_durations,
         sample_rate,
         timbre=None
         ):
-        self.input_refrain = np.asmatrix(input_refrain) # this will enforce 2-dimensionality even if it's with an exception (ValueError)
-        self.durations = np.asarray(durations)
+        self.input_refrain = np.asmatrix(input_refrain) # this will enforce 2-dimensionality via exception - ValueError
+        self.input_durations = np.asarray(input_durations)
         self.sample_rate = sample_rate
         self.timbre = timbre
 
-        if self.input_refrain.shape[1] == self.durations.shape[0]:
-            # if the shape of the input_refrain's first axis and the durations' 0th axis are the same, give us the setup_refrain as a tuple
-            self.setup_refrain = self.input_refrain, self.durations
+        if self.input_refrain.shape[1] != self.input_durations.shape[0]:
+            # ensure they have the same size by tiling, create 'refrain' and 'durations' attributes
+            self.refrain = np.tile(self.input_refrain, len(self.input_durations))
+            self.durations = np.tile(self.input_durations, self.input_refrain.shape[1])
         else:
-            # else, ensure they have the same size by tiling
-            self.setup_refrain = np.tile(self.input_refrain, len(self.durations)), np.tile(self.durations, len(self.input_refrain))
+            self.refrain = self.input_refrain
+            self.durations = self.input_durations
 
-        self.duration_in_samples = self._duration_in_samples(self.input_refrain) # returns a matrix of the input_refrain's durations, but in samples
-        
+        self._vectorized_get_duration_in_samples = np.vectorize(self._get_duration_in_samples)
 
-        @staticmethod
-        def get_durations_samples(frequency, duration_in_seconds, sample_rate):
-            if frequency == 0:
-                return 0
-            cycle_time_samples = sample_rate / frequency
-            num_samples = sample_rate * duration
-            range_cycles = np.ceil(np.arange(0, int(num_samples + cycle_time_samples), cycle_time_samples))
+        self.durations_in_samples = self._vectorized_get_duration_in_samples(self.refrain, self.durations)
 
-            diff_second_to_last, diff_last = abs(num_samples - range_cycles[-2]), abs(num_samples - range_cycles[-1])
+    def _get_duration_in_samples(self, frequency, duration_in_seconds):
+        if frequency == 0:
+            return 0
+        cycle_time_samples = self.sample_rate / frequency
+        num_samples = self.sample_rate * duration_in_seconds
+        range_cycles = np.ceil(np.arange(0, int(num_samples + cycle_time_samples), cycle_time_samples))
 
-            which_cycle = np.argmin([diff_second_to_last, diff_last])
+        diff_second_to_last, diff_last = abs(num_samples - range_cycles[-2]), abs(num_samples - range_cycles[-1])
 
-            return range_cycles[-2] if which_cycle == 0 else range_cycles[-1]
+        which_cycle = np.argmin([diff_second_to_last, diff_last])
 
-        def _duration_in_samples(self):
-            return np.vectorize(get_duration_in_samples)
+        return range_cycles[-2] if which_cycle == 0 else range_cycles[-1]
