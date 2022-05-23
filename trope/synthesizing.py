20fc2fb137cecdf6f66fa5e6fd94f62c0cbde38e
aufdemarbeitsmarkt <37612712+aufdemarbeitsmarkt@users.noreply.github.com>
2022-05-22 21:45:18 -0700

    Refactor synthesizing.py (#1)
    * testing branch + made updates to cycle time + zero crossing
    
    * removed extra notebooks + screenshots
    
    * added sample_len_log to Synthesis
    
    * updates
    
    * created synthesizing_new.py
    
    * completed vectorizing getting the duration in samples
    
    * added Synthesis._intialize_matrix
    
    * working on vectorizing a function that generates a sine wave
    
    * continued work in worksheet.ipynb - looking good
    
    * some trouble iterating, see note in the worksheet cell with out_matrix; should be easy to solve
    
    * added cumsum and sample_boundaries to worksheet
    
    * moved synthesizing.py to scrapbook directory for now, added normalize_output and sum_output arguments to _synthesize() & functionality
    
    * rename synthesizing_new

--- trope/synthesizing.py
+++ trope/synthesizing.py
@@ -5,153 +5,87 @@ from math import ceil
 import numpy as np
 
 
-class Tone:
-
-    default_amplitude = 0.5
-
-    def __init__(
-        self,
-        frequency,
-        duration,
-        sample_rate,
-        amplitude=None
-        ):
-        self.frequency = frequency
-        self.duration = duration
-        self.sample_rate = sample_rate
-        self.duration_in_samples = self._get_duration_in_samples()
-
-        print(f'{self.duration_in_samples=}')
-
-        if amplitude is None:
-            amplitude = self.default_amplitude
-        self.amplitude = amplitude
-
-        self.tone = self._generate_tone()
-
-    def _get_duration_in_samples(self):
-        self.cycle_time = int(self.sample_rate / self.frequency)
-        self.num_samples = int(self.sample_rate * self.duration)
-        self.range_cycles = range(0, self.num_samples + self.cycle_time, self.cycle_time)
-
-        diff_second_to_last, diff_last = abs(self.num_samples - self.range_cycles[-2]), abs(self.num_samples - self.range_cycles[-1])
-
-        which_cycle = np.argmin([diff_second_to_last, diff_last])
-
-        return self.range_cycles[-2] if which_cycle == 0 else self.range_cycles[-1]
-
-    def _generate_tone(self):
-        each_sample = np.arange(self.duration_in_samples)
-        sine = np.sin(2 * np.pi * each_sample * self.frequency / self.sample_rate) * self.amplitude
-        return sine
-
-
 class Synthesis:
 
     def __init__(
         self,
         input_refrain,
-        durations,
+        input_durations,
         sample_rate,
         timbre=None
         ):
-        self.input_refrain = np.asarray(input_refrain)
-        self.durations = np.asarray(durations)
+        self.input_refrain = np.asmatrix(input_refrain) # this will enforce 2-dimensionality via exception - ValueError
+        self.input_durations = np.asarray(input_durations)
         self.sample_rate = sample_rate
         self.timbre = timbre
 
-        self._setup_metadata()
-        print(f'{self.sample_boundaries=}')
+        if self.input_refrain.shape[1] != self.input_durations.shape[0]:
+            # ensure they have the same size by tiling, create 'refrain' and 'durations' attributes
+            self.refrain = np.tile(self.input_refrain, len(self.input_durations))
+            self.durations = np.tile(self.input_durations, self.input_refrain.shape[1])
 
-        self.synthesized_output = self.synthesize()
-
-    def _setup_metadata(self):
-        self.chord_present = any([isinstance(i, tuple) for i in self.input_refrain]) # True or False whether a chord is present
-        self.longest_chord_len = max(len(r) if isinstance(r, tuple) else 1 for r in self.input_refrain)
-        self.timbre_present = self.timbre is not None # same as chord_present, re: timbre
-
-        if len(self.input_refrain) == len(self.durations):
-            self.setup_refrain = self.input_refrain, self.durations
         else:
-            self.setup_refrain =  np.tile(self.input_refrain, len(self.durations)), np.tile(self.durations, len(self.input_refrain))
+            self.refrain = self.input_refrain
+            self.durations = self.input_durations
 
-        self.total_duration = sum(self.setup_refrain[-1]) # duration in seconds; must be created after self.setup_refrain
+        self._vectorized_get_duration_in_samples = np.vectorize(self._get_duration_in_samples)
 
-        self.sample_boundaries = np.cumsum(self.setup_refrain[-1] * self.sample_rate, dtype='int') # the sample boundaries where each note or chord will be set
-        self.sample_boundaries = np.insert(self.sample_boundaries[:-1], 0, 0)
+        self.durations_in_samples = self._vectorized_get_duration_in_samples(self.refrain, self.durations)
 
-        return None
+        self.max_durations_samples = np.max(self.durations_in_samples, axis=0).astype('int')
+
+        _cumsum_max_durations = np.ravel(np.cumsum(self.max_durations_samples))
+        self.sample_boundaries = np.insert(_cumsum_max_durations[:-1], 0, 0)
+
+        self.synthesized_output = self._synthesize()
+
+    def _generate_tone(self, frequency, duration_in_samples, amplitude=0.5, pad_amount=0):
+        each_sample = np.arange(duration_in_samples)
+        sine = np.sin(2 * np.pi * each_sample * frequency / self.sample_rate) * amplitude
+
+        if pad_amount - duration_in_samples > 0:
+            pad_for_each_side = (pad_amount - duration_in_samples) / 2
+            beginning = int(pad_for_each_side)
+            end = ceil(pad_for_each_side)
+            return np.pad(sine, (beginning, end)) # note: also works for rests
+
+        return sine
+
+    def _get_duration_in_samples(self, frequency, duration_in_seconds):
+        if frequency == 0:
+            return 0
+        cycle_time_samples = self.sample_rate / frequency
+        num_samples = self.sample_rate * duration_in_seconds
+        range_cycles = np.ceil(np.arange(0, int(num_samples + cycle_time_samples), cycle_time_samples))
+
+        diff_second_to_last, diff_last = abs(num_samples - range_cycles[-2]), abs(num_samples - range_cycles[-1])
+
+        which_cycle = np.argmin([diff_second_to_last, diff_last])
+
+        return range_cycles[-2] if which_cycle == 0 else range_cycles[-1]
 
     def _initialize_matrix(self):
-        axis0 = self.longest_chord_len * len(self.timbre) if self.timbre_present else self.longest_chord_len
-        axis1 = ceil(self.total_duration) * self.sample_rate
-        return np.zeros((axis0, axis1))
+        '''
+        Sets self.total_duration_in_samples.
+        Returns a matrix for the entire Synthesis object, i.e. allocates space in memory in advance.
+        '''
+        self.total_duration_in_samples = np.sum(self.max_durations_samples, dtype='int')
+        return np.empty((self.refrain.shape[0], self.total_duration_in_samples))
 
-    # def _generate_tone(self, frequency, duration, amplitude=1):
-    #     each_sample = np.arange(ceil(duration * self.sample_rate))
-    #     tone = np.sin(2 * np.pi * each_sample * frequency / self.sample_rate) * amplitude
-    #     return tone
+    def _synthesize(self, normalize_output=True, sum_output=True):
+        output = self._initialize_matrix()
 
-    def _create_nxn_array(self, frequencies, duration):
-        chord = isinstance(frequencies, tuple)
-        if chord and self.timbre_present:
-            axis0 = len(frequencies) * len(self.timbre)
-        elif self.timbre_present:
-            axis0 = len(self.timbre)
-        else:
-            axis0 = len(frequencies)
+        for i,r in np.ndenumerate(self.refrain):
 
-        nxn_array = np.empty((axis0, ceil(duration * self.sample_rate)))
+            tone = self._generate_tone(
+                frequency=r,
+                duration_in_samples=self.durations_in_samples[i], pad_amount=self.max_durations_samples[0,i[1]]
+                )
+            output[i[0], self.sample_boundaries[i[1]] : self.sample_boundaries[i[1]]+tone.size] = tone
 
-        if chord and self.timbre_present:
-            chord_timbre_prod = product(frequencies, self.timbre)
-            for i, (freq, (overtone, amplitude)) in enumerate(chord_timbre_prod):
-                nxn_array[i] = Tone(frequency=freq*overtone, duration=duration, amplitude=amplitude).tone
-
-                # self._generate_tone(freq*overtone, duration=duration, amplitude=amplitude)
-
-        elif self.timbre_present:
-            for i, (overtone, amplitude) in enumerate(self.timbre):
-                tone = Tone(frequency=frequencies*overtone, duration=duration, sample_rate=self.sample_rate, amplitude=amplitude).tone
-
-                # self._generate_tone(frequencies*overtone, duration=duration, amplitude=amplitude)
-
-                nxn_array[i] = tone
-
-        else:
-            for i, freq in enumerate(frequencies):
-                nxn_array[i] = Tone(frequency=freq, duration=duration, sample_rate=self.sample_rate).tone
-                # self._generate_tone(freq, duration=duration)
-
-        return nxn_array
-
-    def synthesize(self, sum_output=True):
-        matrix = self._initialize_matrix()
-        freq, dur = self.setup_refrain
-
-        for i, (f,d) in enumerate(zip(freq, dur)):
-            if f is None: # a rest
-                pass
-
-            elif isinstance(f, tuple) or self.timbre_present:
-                nxn = self._create_nxn_array(f, d)
-                matrix[0:nxn.shape[0], self.sample_boundaries[i]:self.sample_boundaries[i] + nxn.shape[-1]] = nxn
-
-            elif isinstance(f, (float, np.floating, int, np.integer)):
-                tone = Tone(frequency=f, duration=d, sample_rate=self.sample_rate).tone
-
-                # self._generate_tone(f, d)
-
-                ## THERE'S SOME WEIRDNESS HERE - NEED TO GET THIS TO WORK WITH THE NEW TONE CLASS
-
-                matrix[0, self.sample_boundaries[i]:self.sample_boundaries[i] + tone.shape[-1]] = tone
-
-            elif isinstance(f, list):
-                pass # not supporting arpeggios just yet
-
-        normalized_matrix = librosa.util.normalize(matrix, norm=0, axis=0, threshold=None, fill=None)
-
+        if normalize_output:
+            output = librosa.util.normalize(output, norm=0, axis=0, threshold=None, fill=None)
         if sum_output:
-            return np.sum(normalized_matrix, axis=0)
-        else:
-            return normalized_matrix
+            output = np.sum(output, axis=0)
+
+        return output
