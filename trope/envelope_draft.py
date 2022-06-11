a307fbca4a6f7334413b3ab53fc6011e2747d030
Tim Nilsen <nilsentim.e@gmail.com>
2022-06-11 13:17:36 -0700

    moved to linspace to accommodate 0

--- trope/envelope_draft.py
+++ trope/envelope_draft.py
@@ -8,7 +8,7 @@ class Envelope:
 
     attack_level = 1.0
     sustain_level = 0.75
-    quiet_level = 0.001
+    quiet_level = 0.0
 
     def __init__(
         self,
@@ -30,7 +30,7 @@ class Envelope:
     def attack(self):
         attack_range = self.sample_rate * np.linspace(0.001, 5, num=100)
 
-        complete_attack = np.geomspace(
+        complete_attack = np.linspace(
             self.quiet_level,
             self.attack_level,
             num=int(attack_range[self.attack_setting])
@@ -42,7 +42,7 @@ class Envelope:
     def decay(self):
         decay_range = self.sample_rate * np.linspace(0.001, 10, num=100)
 
-        complete_decay = np.geomspace(
+        complete_decay = np.linspace(
             self.attack_level,
             self.sustain_level,
             num=int(decay_range[self.decay_setting])
@@ -64,45 +64,59 @@ class Envelope:
         if max_level is None:
             max_level = self.quiet_level
 
-        release_size = get_release_size(input_signal_size, self.release_setting)
+        release_size = self.get_release_size(input_signal_size, self.release_setting)
 
         release_range = self.sample_rate * np.linspace(0.001, 10, num=100)
-        complete_release = np.geomspace(
+        complete_release = np.linspace(
             min_level,
             max_level,
             num=release_size
             )
-
+        
         return complete_release
 
     @staticmethod
     def get_release_size(input_signal_size, release_point):
         return int(input_signal_size * (release_point/100))
 
-    def generate_envelope_signal(self, input_signal, release_point=20):
+    def generate_envelope_signal(self, input_signal):
         input_signal_size = input_signal.size
         release_size = self.get_release_size(input_signal_size, self.release_setting)
 
         envelope = np.empty(input_signal_size)
 
         if self.attack.size + release_size >= input_signal_size:
-            envelope[:-release_size] = self.attack[:-release_size]
+            # set the attack
+            attack_amt = input_signal_size - release_size
+            envelope[:attack_amt] = self.attack[:attack_amt]
+            # set the release
             envelope[-release_size:] = self.generate_release(
                 input_signal_size,
-                min_level=self.attack[-release_size]
+                min_level=self.attack[attack_amt]
                 )
         elif self.attack.size + self.decay.size + release_size >= input_signal_size:
+            # set the attack
             envelope[:self.attack.size] = self.attack
-            envelope[self.attack.size:-release_size] = self.decay[:-release_size]
+            # set the decay
+            decay_amt = input_signal_size - self.attack.size - release_size
+            envelope[self.attack.size:-release_size] = self.decay[:decay_amt]
+            # set the release
             envelope[-release_size:] = self.generate_release(
                 input_signal_size,
-                min_level=self.decay[-release_size]
+                min_level=self.decay[decay_amt]
                 )
         elif self.attack.size + self.decay.size + self.sustain.size + release_size >= input_signal_size:
+            # set the attack
             envelope[:self.attack.size] = self.attack
-            envelope[self.attack.size:self.decay.size] = self.decay
-            envelope[self.decay.size:-release_size] = self.sustain[:-release_size]
+            # set the decay
+            envelope[self.attack.size:self.attack.size + self.decay.size] = self.decay
+            # set the sustain 
+            sustain_amt = input_signal_size - self.attack.size - self.decay.size - release_size
+            envelope[self.attack.size + self.decay.size:-release_size] = self.sustain[:sustain_amt]
+            # set the release
             envelope[-release_size:] = self.generate_release(
                 input_signal_size,
-                min_level=self.sustain[-release_size]
+                min_level=self.sustain[sustain_amt]
                 )
+
+        return envelope
